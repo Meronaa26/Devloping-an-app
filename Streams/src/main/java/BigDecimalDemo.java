d1876f90d7dc7a41712d2e9d505bc01350bb7d19
merry teweldemedhen <meronaa26@gmail.com>
2022-02-05 11:37:11 -0800

    BigDecimal demo

--- Streams/src/main/java/BigDecimalDemo.java
+++ Streams/src/main/java/BigDecimalDemo.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BigDecimalDemo {
 
     public static void main(String[] args) {
@@ -5,7 +8,26 @@ public class BigDecimalDemo {
         double d2 =174.46;
 
 
-        System.out.println(d1-d2);
+        System.out.println(d1-d2);// you can not to this in real world because the result is a lot of decimals  0.09999999999999432
+
+        BigDecimal b1 = new BigDecimal("174.56");
+        BigDecimal b2 = new BigDecimal("174.46");
+
+        System.out.println(b1.subtract(b2));  //0.10
+
+
+
+        //SCALING AND ROUNDING
+
+        BigDecimal number1  =  new BigDecimal(23.12);
+        System.out.println(number1.setScale(1, RoundingMode.CEILING));
+        System.out.println(number1.setScale(2, RoundingMode.CEILING));
+        System.out.println(number1.setScale(2, RoundingMode.FLOOR));
+
+        System.out.println(new BigDecimal("2").compareTo(new BigDecimal("2")));// kind of the differences 
+        System.out.println(new BigDecimal("2").compareTo(new BigDecimal("3")));
+        System.out.println(new BigDecimal("3").compareTo(new BigDecimal("2")));
+
 
     }
 }
