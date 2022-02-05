35ce87240ef753ee363b0c36b0790ff62aff62bc
merry teweldemedhen <meronaa26@gmail.com>
2022-02-05 11:24:50 -0800

    optional

---
+++ Streams/src/main/java/BigDecimalDemo.java
@@ -0,0 +1,11 @@
+public class BigDecimalDemo {
+
+    public static void main(String[] args) {
+        double d1= 174.56;
+        double d2 =174.46;
+
+
+        System.out.println(d1-d2);
+
+    }
+}
--- Streams/src/main/java/JavaCollectors.java
+++ Streams/src/main/java/JavaCollectors.java
@@ -50,6 +50,30 @@ public class JavaCollectors {
         System.out.println("************* summingInt()   ***********");
       Integer sum =  DishData.getAll().stream()
                                .collect(Collectors.summingInt(Dish::getCalories));
+        System.out.println(sum);
+      //averagingInt(ToIntFunction)
+        System.out.println("************* averageingInt()   ***********");
+        Double caloriesAvg =DishData.getAll().stream().collect(Collectors.averagingInt(Dish::getCalories));
+        System.out.println(caloriesAvg);
+
+        //joing() : is used to join various elements of a character  or string array into a single string object
+        System.out.println("************* joining()   ***********");
+        List<String> courses = Arrays.asList("java","js" ,"ts");
+       String str= courses.stream().collect(Collectors.joining(""));
+        System.out.println(str);
+
+        //partitioningBy() : is used to partition a stream of objects(set of elements )based on given predicate
+        //  MAP<Boolean, List<Dish>>veggieDish= DishData.getAll().stream().collect(Collectors.partitioningBy(Dish::isVegetarian));
+        System.out.println();
+
+
+        //groupingBy(): is used for gruoping by
+        System.out.println("************* groupingby()   ***********");
+        Map<Type ,List<Dish>>dishType = DishData.getAll().stream().collect(Collectors.groupingBy(Dish::getType));
+        System.out.println(dishType);
+
+
+
 
 
 
